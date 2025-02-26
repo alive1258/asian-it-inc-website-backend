2be8ac45a525c40eaa3dddfd2e57a16747e81ae5
alive1258 <javashazib@gmail.com>
2025-02-26 11:47:20 +0600

    create user dto done

--- src/app/modules/users/dto/create-user.dto.ts
+++ src/app/modules/users/dto/create-user.dto.ts
@@ -1,12 +1,73 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsLowercase,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   /**
-   * First Name
+   * Name
    */
   @ApiProperty({
-    description: 'First Name',
-    example: 'Nayeem',
+    description: 'Full Name of User',
+    example: 'John Doe',
+    maxLength: 100,
+    required: false,
   })
-  first_name: string;
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  name?: string;
+
+  /**
+   * Mobile Number
+   */
+  @ApiProperty({
+    description: 'Mobile Number of User',
+    example: '+1 123-456-7890',
+    maxLength: 15,
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(15)
+  @Matches(/^\+?[1-9]\d{1,14}$/)
+  mobile?: string;
+
+  /**
+   * Email
+   */
+  @ApiProperty({
+    description: 'Email of User',
+    example: '2EYw0@example.com',
+    required: true,
+  })
+  @IsString()
+  @IsEmail()
+  @IsLowercase()
+  @IsNotEmpty()
+  @MaxLength(255)
+  email: string;
+
+  /**
+   * Password
+   */
+  @IsString()
+  @IsOptional()
+  @MinLength(6)
+  @MaxLength(255)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password must be at least 6 characters long, include one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  password?: string;
 }
